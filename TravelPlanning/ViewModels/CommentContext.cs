f84e4754c8d8346db9f5d2d5f56194408971e7b4
yen <yen@LAPTOP-JR0ECM7S>
2026-03-30 20:39:13 +0800

    optomize comment view and toggle button

--- TravelPlanning/ViewModels/CommentContext.cs
+++ TravelPlanning/ViewModels/CommentContext.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using System.Xml.Linq;
 using TravelPlanning.Utilities.Interfaces;
 using static GoogleMap.SDK.Contract.GoogleMapAPI.Models.Place.PlaceDetail.PlaceDetailResModel;
@@ -14,13 +15,36 @@ namespace TravelPlanning.ViewModels
     [AddINotifyPropertyChangedInterface]
     internal class CommentContext : INavigationAware
     {
+        private const int PageSize = 3;
 
-        public ObservableCollection<Review> Comments { get; set; } = new ObservableCollection<Review>();
+        private List<Review> _allComments = new List<Review>();
 
+        public ObservableCollection<Review> DisplayedComments { get; set; } = new ObservableCollection<Review>();
+
+        public bool HasMoreComments { get; set; } = false;
+
+        public ICommand ShowMoreCommand { get; }
+
+        public CommentContext()
+        {
+            ShowMoreCommand = new RelayCommand(ShowMore);
+        }
+
+        private void ShowMore()
+        {
+            int currentCount = DisplayedComments.Count;
+            var next = _allComments.Skip(currentCount).Take(PageSize);
+            foreach (var review in next)
+                DisplayedComments.Add(review);
+
+            HasMoreComments = DisplayedComments.Count < _allComments.Count;
+        }
 
         public void DataAware(object data)
         {
-            Comments = new ObservableCollection<Review>((Review[])data);
+            _allComments = new List<Review>((Review[])data);
+            DisplayedComments = new ObservableCollection<Review>(_allComments.Take(PageSize));
+            HasMoreComments = _allComments.Count > PageSize;
         }
     }
 }
